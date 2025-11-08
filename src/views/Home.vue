9802d04ca1c939bb143417dab32d7699c3639d13
unknown <1791611508@qq.com>
2025-11-08 12:54:19 +0800

    travis

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref , onMounted } from 'vue';
+
+  const cavsDom = ref<any>(null)
+
+  onMounted(() => {
+    console.log(cavsDom.value)
+  })
+</script>
+<template>
+  <div class="main_container">
+    <canvas ref="cavsDom"></canvas>
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '*': path.resolve('')
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 9000,
+    open: true,
+  }
 })
